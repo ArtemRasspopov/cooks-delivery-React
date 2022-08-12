82ec1b49045e5da254ab90052b0cf0ddd647a170
ArtemCaster <97943938+ArtemCaster@users.noreply.github.com>
2022-08-12 14:19:41 +0300

    adaptivity content

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import style from './Content.module.scss'
-import Сategories from '../categories/Сategories'
-import Sorting from '../sorting/Sorting'
-import ProductCurd from '../productCurd/ProductCurd'
-import Footer from '../footer/Footer'
+import React from "react";
+import style from "./Content.module.scss";
+import Сategories from "../categories/Сategories";
+import Sorting from "../sorting/Sorting";
+import ProductCurd from "../productCurd/ProductCurd";
+import Footer from "../footer/Footer";
 
 const Content = () => {
   return (
     <div className={style.content}>
-      <div className='container'>
-        <Сategories/>
-        <Sorting/>
+      <div className="container">
+        <Сategories />
+        <Sorting />
         <div className={style.product_list}>
-          <ProductCurd/>
-          <ProductCurd/>
-          <ProductCurd/>
-          <ProductCurd/>
-          <ProductCurd/>
+          <ProductCurd />
+          <ProductCurd />
+          <ProductCurd />
+          <ProductCurd />
+          <ProductCurd />
         </div>
-        <Footer/>
+        <Footer/> 
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
